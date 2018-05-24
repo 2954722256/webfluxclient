36b8acdb04b6eaf8f5adc9d3e10805e4961a19c4
dodo_lihao李灏 <2954722256@qq.com>
2018-05-24 09:30:46 +0800

    annotation
    annotation and bean

---
+++ src/main/java/cn/dodo/webfluxclient/bean/User.java
@@ -0,0 +1,21 @@
+package cn.dodo.webfluxclient.bean;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    private String id;
+
+    private String name;
+
+    private int age;
+
+}
+
